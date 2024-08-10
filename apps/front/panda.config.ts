bf5aa1eeb61383a2d689d2b6e7eb0892dfd8d6b6
TheTensaii <f.salimou@hotmail.fr>
2024-08-10 12:43:31 -0400

    chore(front): add panda css

---
+++ apps/front/panda.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {},
+  },
+  
+  jsxStyleProps:'minimal',
+  jsxFramework: 'react',
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
